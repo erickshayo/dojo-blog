ac3f9a73b92ec5f06245d790d2d4a55cd259cb32
erickshayo <erickshayo306@gmail.com>
2023-11-15 21:27:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ import Navbar from './component/Navbar';
+ import Home from './component/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
